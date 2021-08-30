1456dfde542f4876e27228d177bd3ccbb7b30a30
magnus <x@x.com>
2021-08-30 23:34:10 +0200

    moved key functions to library and separate run code in seperate file run_experiment

--- img_utils.py
+++ img_utils.py
@@ -1,9 +1,53 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Aug 27 08:43:11 2021
-
-@author: lilledah
+Various functions that can be used to extract computation from images
+@author: Magnus Lilledahl
 """
+import numpy as np
+
+#Function computes the difference in grey levels to neighbours at different distances from a pxiel and 
+#returns a hyperspectral image with the differences at different distances.
+#Must 
+def neighbour_diff(img,levels,isotropic=False):
+    #check if ndarray
+    if not(isinstance(img,np.ndarray)):
+        pass
+        #throw exeption
+    
+    #check image is NxNx1
+    if (len(img.shape) != 2):
+        pass
+        #throw exception
+
+    #compute num_features.
+    if isotropic:
+        num_features = levels
+    else:
+        num_features = num_neighbours(levels) + 1 #depends on levels
+    #initalize data array
+    data = np.zeros(list(img.shape) + [num_features]) #array to hold data
+    data[:,:,0] = img 
+
+    #Compute data
+    for x,y in np.ndindex(img.shape):
+        first = levels
+        last = img.shape[1]-levels
+        if x >= first and x <= last and y >= first and y <= last: #This could be improved...
+            coord = (x,y)        
+            pos = 0
+            for level in range(1,levels+1):
+                dI = []
+                ncoords = neighbour(x, y, level)
+                for ncoord in ncoords:
+                    dI.append(img[coord]-img[tuple(ncoord)])
+                if isotropic:
+                    dI = [sum(dI)]
+                data[x,y,pos+1:pos+len(dI)+1] = dI           
+                pos = pos + len(dI)
+    data = data[levels:-levels,levels:-levels,:] #remove padding
+    return data
+
 #Returns coordinates of neighbours at certain distances
 #...can this not be hard-coded?...
 def neighbour(x,y,level):
---
+++ run_experiment.py
@@ -0,0 +1,69 @@
+#Script which runs an experiment, that is, an analysis og data using a set of parameters.
+#Data are stored in a run<#>-folder (an experiment) together with the results 
+#Load data to analyse (should be stored in run folder - to ensure data is constant)
+
+testing = True #Set false to run an experiment where data is to be stored.
+
+#LIBRARIES
+from skimage import io
+from skimage.transform import resize
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import numpy as np
+import tkinter as tk
+import img_utils as utils
+
+#DATA
+#fname = 'chordae.jpg'
+fname = 'cartilage_surface.jpg' #data that are used in the analysis should be stored with analysis data
+img = io.imread(fname)
+
+#PARAMETERS
+#Set experiment parameters
+if testing: #testing parameters
+    img_shape = [100,100] #resample image to this shape
+    levels = 2 #separation perhaps better name.
+    isotropic = False #if 1 sum all intensity differences at specific level
+    num_clusters = 2
+else: #run parameters
+    pass
+
+#PREPROCESSING (select subset, ...)
+img = img[:,:,0] #Select channel 
+img = resize(img,img_shape) #resample 
+
+#ANALYSIS
+data = utils.neighbour_diff(img, levels, isotropic)
+data_flat = data.reshape((data.shape[0]*data.shape[1],data.shape[2])) #flatten
+km = KMeans(n_clusters=num_clusters) #Perform K-means clustering
+km.fit(data_flat)
+labels = km.labels_.reshape(data.shape[0],data.shape[1]) #reshape to image
+
+#RESULTS
+#Results
+if testing: #Show results
+    pass
+    #Visualization
+    fig = plt.figure()
+    ax = fig.add_subplot(121)
+    ax.imshow(data[:,:,0])
+    ax.set_title('Original')
+    ax.set_axis_off()
+    ax = fig.add_subplot(122)
+    ax.imshow(labels)
+    ax.set_title('Clusters')
+    ax.set_axis_off()
+    fig.text(.05,.05,"{:d} clusters".format(num_clusters)\
+            + "\n{:d} levels".format(levels)\
+            + "\nIsotropic: {!r}".format(bool(isotropic))\
+            + "\n{:d} Pixels".format(img_shape[0])
+            + "\nFile: " + fname)
+    run_fname = "clusters_" + "l" + str(levels) + "_c" + str(num_clusters) + "_i" + str(isotropic)
+    plt.show()
+    #plt.savefig("experiments/run1/" + run_fname)
+
+else: #Store results
+    #Store commit hash of current code in readme file together with repository
+    #Place original data files together with the results
+    #e.g. numpy.save
+    pass
